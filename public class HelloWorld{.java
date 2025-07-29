5e7a68674a962feac41eb08b2a35317cb10a621d
nidhi111312 <nidhithakur111312@gmail.com>
2025-07-29 23:36:17 +0530

    Add files via upload

---
+++ public class HelloWorld{.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(string[] args){
+        system.out.println("Hello, World1!");
+        system.out.println("Welcome to Java Programming Laboratory");
+        
+    }
+}
